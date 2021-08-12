f87442019f3afbd6f8f2e5c9e1bd828cfb1d5c4b
b0218483 <abhishek.sukhija@airtel.com>
2021-08-12 15:51:49 +0530

    Classwork Class 10

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
-import HP from "./homePage";
-import TP from "./testPage";
-import TDA from "./todoApp";
+export {default as HomePage} from './homePage';
+export {default as TestPage} from './testPage';
+export {default as TodoApp} from './todoApp';
 
-export const HomePage = HP;
-export const TestPage = TP;
-export const TodoApp = TDA;
+// OR
+// import HP from "./homePage";
+// import TP from "./testPage";
+// import TDA from "./todoApp";
+//
+// export const HomePage = HP;
+// export const TestPage = TP;
+// export const TodoApp = TDA;
